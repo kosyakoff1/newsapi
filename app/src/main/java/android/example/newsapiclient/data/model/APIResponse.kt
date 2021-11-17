7942994b0833bcb9486ab8a683ea8a5180774b8a
Alexey Sondor <>
2021-11-17 21:24:59 +0700

    added use case and repository interface

---
+++ app/src/main/java/android/example/newsapiclient/data/model/APIResponse.kt
@@ -0,0 +1,13 @@
+package android.example.newsapiclient.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class APIResponse(
+    @SerializedName("articles")
+    val articles: List<Article>,
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("totalResults")
+    val totalResults: Int
+)
