fb99eea9c7d5ccf74c3905a6b932b19cfab74f83
kosyakoff1 <asondor@salusoptima.com>
2021-11-21 11:24:25 +0700

    Added nav grapsh and bottom bar with menu

--- app/src/main/java/android/example/newsapiclient/MainActivity.kt
+++ app/src/main/java/android/example/newsapiclient/MainActivity.kt
@@ -1,11 +1,24 @@
 package android.example.newsapiclient
 
+import android.example.newsapiclient.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigation_fragment_container_view) as NavHostFragment
+        binding.bnvNews.setupWithNavController(
+            navHostFragment.navController
+        )
     }
 }
