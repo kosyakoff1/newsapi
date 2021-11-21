d75fe3e3a51ae4b830f0df41433f5f8ac5dfb7b4
kosyakoff1 <asondor@salusoptima.com>
2021-11-21 14:44:51 +0700

    Added recycler view, diff util, news fragment

--- app/src/main/java/android/example/newsapiclient/MainActivity.kt
+++ app/src/main/java/android/example/newsapiclient/MainActivity.kt
@@ -1,15 +1,27 @@
 package android.example.newsapiclient
 
 import android.example.newsapiclient.databinding.ActivityMainBinding
+import android.example.newsapiclient.presentation.viewmodel.NewsViewModel
+import android.example.newsapiclient.presentation.viewmodel.NewsViewModelFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    //better use  val model: MyViewModel by viewModels()
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,5 +32,7 @@ class MainActivity : AppCompatActivity() {
         binding.bnvNews.setupWithNavController(
             navHostFragment.navController
         )
+
+        viewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
     }
 }
---
+++ app/src/main/java/android/example/newsapiclient/presentation/adapter/NewsAdapter.kt
@@ -0,0 +1,59 @@
+package android.example.newsapiclient.presentation.adapter
+
+import android.example.newsapiclient.data.model.Article
+import android.example.newsapiclient.databinding.NewsListItemBinding
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+    private val callback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, callback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val binding =
+            NewsListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.bind(article)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+
+    inner class NewsViewHolder(private val binding: NewsListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(article: Article) {
+            with(binding) {
+                tvTitle.text = article.title
+                tvDescription.text = article.description
+                tvPublishedAt.text = article.publishedAt
+                tvSource.text = article.source.name
+                Glide.with(ivArticleImage.context)
+                    .load(article.urlToImage)
+                    .into(ivArticleImage)
+            }
+
+
+        }
+    }
+}
